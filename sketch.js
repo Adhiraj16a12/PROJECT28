80485cba566109b90cc3271e8dd01e1d8f9f246d
Adhiraj16a12 <64204448+Adhiraj16a12@users.noreply.github.com>
2020-05-17 09:53:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,105 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var chain;
 var engine, world;
+var holder,ball,ground;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
 
 
+  var holder_options={
+    isStatic: true
+  }
 
-function setup(){
-    var canvas = createCanvas(800,600);
-    engine = Engine.create();
-    world = engine.world;   
-   
-    ball = new Ball (100,100,20,20);
-    log = new Log(400, 200, 300, 30);
-    chain = new Chain(log.body, ball.body);
+  ground = Bodies.rectangle(200,330,400,20,ground_options)
+  World.add(world,ground);
+
+holder = Bodies.rectangle(200,100,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-     
-    if(keyCode === 32){
-      ball.MoveWithMouse();
-    }
+ball  = Bodies.circle(220,200,40,ball_options);
+World.add(world,ball);
 
-    
-    textSize(30);
-    
-    text("Press space to move the bob", 90, 40);
-    text("PRESS ENTER TO LEAVE THE BOB", 100, 80);
-    
-    ball.display();
-    log.display();
-    chain.display();
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.004,
+  length : 100
 }
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("pink");
+}
+
+
+function draw() {
+  background(123); 
+  Engine.update(engine);
+
+
+  text("Press space bar to oscillate the pendulam to left and right with mouse",10,20);
+  text("Press Enter to stop the Pendulum from oscillating",100,50);
+  text("Press Shift To calmly move it without your control",100,80);
+
+
+
+
+
+  fill ("blue");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+fill("green");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(8);
+stroke("white");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+
+
+
+if(keyCode===32){
+ball.position.y = mouseY;
+ball.position.x = mouseX;
+}
+
+else if (keyCode === ENTER){
+ball.position.x = 200;
+
+}
+
+}
+
+
+
+
+
+
+
+
